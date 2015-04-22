843d31a31de52291d6a3876b4ac352a47d7c97db
Kyle Maurer <kyle@realbigmarketing.com>
2015-04-21 22:56:35 -0400

    Added readme, cleaned up some code and added a little error / conflict handling.

---
+++ README.md
@@ -0,0 +1,13 @@
+WPTV Plugin
+===========
+
+### About
+
+The WPTV plugin is designed to help users better learn, understand and utilize the WordPress admin by providing relevant,
+contextual videos from [WordPress.tv](http://wordpress.tv). These videos will be presented dynamically based on the
+screen the user is currently viewing and will be available within the contextual help menu.
+
+### Notes
+
+This plugin is in active development. Pull requests are absolutely welcome as is input from anyone via the
+[issues](https://github.com/brashrebel/wptv/issues) for this repository.
--- wptv.php
+++ wptv.php
@@ -10,54 +10,79 @@ Author URI: http://kyleblog.net
 License: GPL2
 */
 
+if ( class_exists( 'wptv' ) ) {
+	return;
+}
 /**
  * Class wptv
  */
 class wptv {
 
+	/**
+	 * Array of all the admin screens and the tag for appropriate videos
+	 * @var array
+	 */
 	public $places = array(
 		array(
 			'screen' => 'tools',
-			'tag' => 'api',
+			'tag'    => 'import',
 		),
 		array(
 			'screen' => 'plugins',
-			'tag' => 'plugins'
+			'tag'    => 'plugins'
+		),
+		array(
+			'screen' => 'dashboard',
+			'tag'    => 'dashboard'
+		),
+		array(
+			'screen' => 'update-core',
+			'tag'    => 'update'
+		),
+		array(
+			'screen' => 'widgets',
+			'tag'    => 'widgets'
 		),
 	);
+
 	/**
 	 * Initialize all the things
 	 */
 	public function __construct() {
-		add_action( 'admin_notices', array( $this, 'request' ) );
 		add_action( 'current_screen', array( $this, 'add_help_tab' ) );
 		add_action( 'admin_init', array( $this, 'style' ) );
 	}
 
 	public function style() {
-		wp_register_style( 'wptv', plugins_url( 'assets/style.css', __FILE__ ), array(), '0.1');
+		wp_register_style( 'wptv', plugins_url( 'assets/style.css', __FILE__ ), array(), '0.1' );
 	}
 
 	/**
-	* request
-	*/
+	 * @param $tag
+	 *
+	 * @return array|mixed|string|WP_Error
+	 */
 	public function request( $tag ) {
-		$url = 'http://wordpress.tv/?wptvapi=videos.json&posts_per_page=3&tag=';
+		$url     = 'http://wordpress.tv/?wptvapi=videos.json&posts_per_page=3&tag=';
 		$request = wp_remote_get( $url . $tag );
 		$request = wp_remote_retrieve_body( $request );
 		$request = json_decode( $request );
+
 		return $request;
 	}
 
+	/**
+	 * Creates a Videos tab in the help menu for screens included in $places
+	 */
 	public function add_help_tab() {
 		$screen = get_current_screen();
 		foreach ( $this->places as $place ) {
 			if ( $place['screen'] == $screen->base ) {
 				wp_enqueue_style( 'wptv' );
 				$screen->add_help_tab( array(
-						'id' => 'videos',
-						'title' => 'Videos',
-						'content' => '',
+						'id'       => 'videos',
+						'title'    => 'Videos',
+						'content'  => '',
 						'callback' => array( $this, 'display' ),
 					)
 				);
@@ -65,12 +90,15 @@ class wptv {
 		}
 	}
 
+	/**
+	 * Displays the videos inside the help menu
+	 */
 	public function display() {
 		$screen = get_current_screen();
 		foreach ( $this->places as $place ) {
 			if ( $place['screen'] == $screen->base ) {
 				$videos = $this->request( $place['tag'] );
-				if ( $videos ) {
+				if ( ! is_wp_error( $videos ) ) {
 					echo '<ul class="wptv">';
 					foreach ( $videos->videos as $video ) {
 						echo '<li>';
@@ -87,4 +115,5 @@ class wptv {
 	}
 
 }
+
 $wptv = new wptv();
